4f9abb6889724bdf9f6a5777e5874b82b70becb9
Alexandre Gressier <alexandre@gressier.dev>
2021-08-20 21:09:26 +0200

    Implement the Pick Players fragment

--- app/src/main/java/dev/gressier/pennydrop/fragments/PickPlayersFragment.kt
+++ app/src/main/java/dev/gressier/pennydrop/fragments/PickPlayersFragment.kt
@@ -1,11 +1,11 @@
 package dev.gressier.pennydrop.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dev.gressier.pennydrop.R
+import androidx.fragment.app.Fragment
+import dev.gressier.pennydrop.databinding.FragmentPickPlayersBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,8 +34,8 @@ class PickPlayersFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pick_players, container, false)
+        val binding = FragmentPickPlayersBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
